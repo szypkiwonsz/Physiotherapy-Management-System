48b6590d60796cb91a3d49d2a9070dc36d87c243
szypkiwonsz <szypkiwonsz@github.com>
2020-03-13 20:04:47 +0100

    Name changed

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     is_patient = models.BooleanField(default=False)
-    is_teacher = models.BooleanField(default=False)
+    is_office = models.BooleanField(default=False)
 
     email = models.EmailField("Email adress", unique=True)
 
