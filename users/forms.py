9d5b4cdc8d5c7355f9fa006d0f557e95cab91999
szypkiwonsz <szypkiwonsz@github.com>
2020-03-15 20:57:08 +0100

    Added forms for signup

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,39 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
 from .models import User
 
 
-class OfficeSignUpForm(UserCreationForm):
+class MultipleForm(forms.Form):
+    action = forms.CharField(max_length=60, widget=forms.HiddenInput())
+
+
+class OfficeSignUpForm(UserCreationForm, MultipleForm):
+
+    email = forms.CharField(widget=forms.EmailInput)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(OfficeSignUpForm, self).__init__(*args, **kwargs)
+
+        for field_name in ['email', 'password1', 'password2']:
+            self.fields[field_name].help_text = None
+
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2']
+
+
+class PatientSignUpForm(UserCreationForm, MultipleForm):
+
+    email = forms.CharField(widget=forms.EmailInput)
+
+    def __init__(self, *args, **kwargs):
+        super(PatientSignUpForm, self).__init__(*args, **kwargs)
+
+        for field_name in ['email', 'password1', 'password2']:
+            self.fields[field_name].help_text = None
+
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
