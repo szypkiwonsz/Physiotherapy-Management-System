2036843e50136f5a40430d5f38ac1dd74224fc26
szypkiwonsz <szypkiwonsz@github.com>
2020-03-15 20:57:28 +0100

    Commit

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     is_patient = models.BooleanField(default=False)
     is_office = models.BooleanField(default=False)
 
-    email = models.EmailField("Email adress", unique=True)
+    email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
