a9dd9fced73e4157585be8c6f637512deaf87b8b
szypkiwonsz <szypkiwonsz@github.com>
2020-03-15 20:54:46 +0100

    Added signup view

--- users/views/offices.py
+++ users/views/offices.py
@@ -1,13 +1,47 @@
-from django.views.generic import CreateView
-from users.models import User
-from users.forms import OfficeSignUpForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import OfficeSignUpForm, PatientSignUpForm
+
+from users.multiforms import MultiFormsView
 
 
-class OfficeSignUpView(CreateView):
-    model = User
-    form_class = OfficeSignUpForm
-    template_name = 'users/signup_form.html'
+# def form_redir(request):
+#     return render(request, 'users/signup_form.html')
 
-    def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'office'
-        return super().get_context_data(**kwargs)
+
+def multiple_forms(request):
+    if request.method == 'POST':
+        office_form = OfficeSignUpForm(request.POST)
+        patient_form = PatientSignUpForm(request.POST)
+        if office_form.is_valid() or patient_form.is_valid():
+            return HttpResponseRedirect(reverse('form-redirect'))
+    else:
+        office_form = OfficeSignUpForm()
+        patient_form = PatientSignUpForm()
+
+    return render(request, 'users/signup_form.html', {
+        'office': office_form,
+        'patient': patient_form,
+    })
+
+
+class SignUpView(MultiFormsView):
+    template_name = "users/signup_form.html"
+    form_classes = {'office': OfficeSignUpForm,
+                    'patient': PatientSignUpForm,
+                    }
+
+    def office_form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        form_name = form.cleaned_data.get('action')
+        return HttpResponseRedirect(self.get_success_url(form_name))
+
+    def patient_form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        form_name = form.cleaned_data.get('action')
+        form = form.save(commit=False)
+        form.is_patient = True
+        form.save()
+        return HttpResponseRedirect(self.get_success_url(form_name))
