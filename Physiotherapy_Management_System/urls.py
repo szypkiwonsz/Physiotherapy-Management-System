fcf520c8a5229ed953618f7b8a20fee10f5a33fd
szypkiwonsz <szypkiwonsz@github.com>
2020-03-16 18:46:01 +0100

    Added url patterns

--- Physiotherapy_Management_System/urls.py
+++ Physiotherapy_Management_System/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from users.views import offices
+from django.urls import path, include
+from users.views import signup, login
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', include('home_page.urls')),
+    path('panel/', login.check_user, name='panel'),
+    path('patient/', include('patient_panel.urls')),
+    path('office/', include('office_panel.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/signup/office/', offices.SignUpView.as_view(), name='signup'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
+    path('accounts/signup/', signup.SignUpView.as_view(), name='signup'),
 ]
