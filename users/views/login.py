2ca70348e2ee6f0ace2e34632b904027e31062d8
szypkiwonsz <szypkiwonsz@github.com>
2020-03-22 16:47:57 +0100

    Added checking if user has activated his account

--- users/views/login.py
+++ users/views/login.py
@@ -1,5 +1,8 @@
-from django.shortcuts import redirect
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.views import View
 
 
 def check_user(request):
@@ -9,4 +12,35 @@ def check_user(request):
         else:
             return redirect('patient-home')
     else:
+        messages.warning(request, "Musisz się zalogować, aby mieć dostęp do panelu.")
         return redirect('login')
+
+
+class Login(View):
+    form_class = AuthenticationForm
+    initial = {'form': 'form'}
+    template_name = 'users/login.html'
+
+    def get(self, request):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    @staticmethod
+    def post(request):
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                email = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(email=email, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(request, f"Poprawnie zalogowałeś się.")
+                    return redirect('panel')
+                else:
+                    messages.warning(request, "Niepoprawny email lub hasło, bądź twoje konto nie zostało aktywowane.")
+            else:
+                messages.warning(request, "Niepoprawny email lub hasło, bądź twoje konto nie zostało aktywowane.")
+        else:
+            form = AuthenticationForm()
+        return render(request=request, template_name="users/login.html", context={"form": form})
