0ce5aa9ace6a621c5670a146739b45786eb233c0
szypkiwonsz <szypkiwonsz@github.com>
2020-03-23 20:27:21 +0100

    Commit

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.make_appointment, name='appointments-make_appointment'),
+    path('', views.MakeAppointment.as_view(), name='make-appointment'),
     path('cancel_appointment/', views.cancel_appointment, name='appointments-cancel_appointment')
 ]
