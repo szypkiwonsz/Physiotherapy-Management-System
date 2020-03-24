3b3605567a7dd98509f35362815d945e5bb93d46
szypkiwonsz <kacpersawicki321@gmail.com>
2020-03-24 13:58:43 +0100

    Added displaying name of the office assigned to the patient

--- patient_panel/views.py
+++ patient_panel/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
-from django.utils.decorators import method_decorator
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from users.models import Office
 
 from users.decorators import patient_required
 
 # Create your views here.
 @patient_required(login_url='login')
 def patient_home(request):
-    return render(request, 'patient_panel/patient_home.html')
+    author = request.user
+
+    context = {
+        'offices': Office.objects.filter(user__patients__email=author),
+    }
+    return render(request, 'patient_panel/patient_home.html', context)
