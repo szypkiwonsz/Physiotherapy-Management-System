b1a689ea7594f5894714c5395dfbb75ad00a57a9
szypkiwonsz <kacpersawicki321@gmail.com>
2020-03-25 18:38:42 +0100

    Added patient and office model

--- users/models.py
+++ users/models.py
@@ -6,7 +6,24 @@ class User(AbstractUser):
     is_patient = models.BooleanField(default=False)
     is_office = models.BooleanField(default=False)
 
+    username = models.CharField(max_length=40, unique=False, default='')
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class Patient(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patients')
+    first_name = models.CharField(max_length=20, unique=False, default='')
+    last_name = models.CharField(max_length=40, unique=False, default='')
+    email = models.EmailField(unique=True, default='')
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Office(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=50)
+    patients = models.ManyToManyField(Patient)
