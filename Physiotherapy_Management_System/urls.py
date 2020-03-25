abeca6d90cf1b8b2128d9e7dcc6a1f8ae0b96cb5
szypkiwonsz <kacpersawicki321@gmail.com>
2020-03-25 18:36:35 +0100

    Code fixes and email activation url added

--- Physiotherapy_Management_System/urls.py
+++ Physiotherapy_Management_System/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from users.views import signup, login
+from django.urls import path, include, re_path
+from users.views import signup, login, activate_account
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -9,7 +9,10 @@ urlpatterns = [
     path('patient/', include('patient_panel.urls')),
     path('office/', include('office_panel.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('accounts/login/', login.Login.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
-    path('accounts/signup/', signup.SignUpView.as_view(), name='signup'),
+    path('accounts/signup/patient/', signup.RegisterPatient.as_view(), name='patient-signup'),
+    path('accounts/signup/office/', signup.RegisterOffice.as_view(), name='office-signup'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            activate_account.activate, name='activate')
 ]
