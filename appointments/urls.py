5241c69c837e526d07f33cd74984f0932510daed
szypkiwonsz <kacpersawicki321@gmail.com>
2020-03-26 13:46:22 +0100

    Added select and make appointment url

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MakeAppointment.as_view(), name='make-appointment'),
+    path('', views.AppointmentListView.as_view(), name='appointments-home'),
+    path('select/', views.SelectOffice.as_view(), name='appointments-select'),
+    path('<int:pk>/', views.MakeAppointment.as_view(), name='appointments-make-appointment'),
     path('cancel_appointment/', views.cancel_appointment, name='appointments-cancel_appointment')
 ]
