96286d8d4becda5c86b046e146ca22ca9e8bdda2
szypkiwonsz <kacpersawicki321@gmail.com>
2020-03-26 17:48:01 +0100

    Added urls for PatientListView, PatientCreateView, PatientUpdateView and PatientDeleteView

--- office_panel/urls.py
+++ office_panel/urls.py
@@ -1,7 +1,10 @@
-from django.urls import include, path
-from . import views
+from django.urls import path
+from office_panel.views import home, patient
 
 urlpatterns = [
-    path('', views.office_home, name='office-home'),
-
+    path('', home.PatientListView.as_view(), name='office-home'),
+    path('patients/', patient.PatientListView.as_view(), name='office-patients'),
+    path('patients/add/', patient.PatientCreateView.as_view(), name='office-patient-add'),
+    path('patients/<int:pk>/', patient.PatientUpdateView.as_view(), name='office-patient-change'),
+    path('patients/<int:pk>/delete/', patient.PatientDeleteView.as_view(), name='office-patient-delete-confirm'),
     ]
