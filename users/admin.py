0f09550e48f3a26513c870268005435ea625e2f7
szypkiwonsz <kacpersawicki321@gmail.com>
2020-03-28 19:02:39 +0100

    Added models to admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Patient, Office
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Patient)
+admin.site.register(Office)
