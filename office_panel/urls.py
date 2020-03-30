9caa1411747867af7122524d0585d350951a63b1
szypkiwonsz <kacpersawicki321@gmail.com>
2020-03-30 23:13:31 +0200

    Added appointment urls

--- office_panel/urls.py
+++ office_panel/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from office_panel.views import home, patient
+from office_panel.views import home, patient, appointment
 
 urlpatterns = [
     path('', home.PatientListView.as_view(), name='office-home'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('patients/add/', patient.PatientCreateView.as_view(), name='office-patient-add'),
     path('patients/<int:pk>/', patient.PatientUpdateView.as_view(), name='office-patient-change'),
     path('patients/<int:pk>/delete/', patient.PatientDeleteView.as_view(), name='office-patient-delete-confirm'),
+    path('appointments/', appointment.AppointmentListView.as_view(), name='office-appointments'),
+    path('appointments/<int:pk>/', appointment.AppointmentUpdateView.as_view(), name='office-appointment-change'),
     ]
