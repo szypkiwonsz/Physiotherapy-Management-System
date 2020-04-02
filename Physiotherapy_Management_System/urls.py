2aae4d8128b27bfb7d747540ab418bd34ecd9bd5
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-02 22:24:44 +0200

    Code improvement

--- Physiotherapy_Management_System/urls.py
+++ Physiotherapy_Management_System/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('office/', include('office_panel.urls')),
     path('admin/', admin.site.urls),
     path('accounts/login/', login.Login.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('accounts/signup/patient/', signup.RegisterPatient.as_view(), name='patient-signup'),
     path('accounts/signup/office/', signup.RegisterOffice.as_view(), name='office-signup'),
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
