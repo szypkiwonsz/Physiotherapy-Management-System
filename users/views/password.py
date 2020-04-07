b1bd604282f022abfb3ecfe1ea51716afd506c33
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-07 14:05:30 +0200

    Commit

---
+++ users/views/password.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import (get_user_model)
+from django.contrib.auth.views import PasswordResetConfirmView
+from users.forms import NewSetPasswordForm
+
+UserModel = get_user_model()
+
+
+class NewPasswordResetConfirmView(PasswordResetConfirmView):
+    form_class = NewSetPasswordForm
