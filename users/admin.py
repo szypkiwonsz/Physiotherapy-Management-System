e4c454bc9c2a815ee4c70c416d58f01b1f22d54a
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-13 13:32:39 +0200

    Added Profile to admin site

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Patient, Office
+from .models import User, Patient, Office, Profile
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Patient)
 admin.site.register(Office)
+admin.site.register(Profile)
