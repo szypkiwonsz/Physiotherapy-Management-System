2690790504188cfb8ca94de0f974f3fa8d41eff4
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-13 13:31:23 +0200

    Added Profile model

--- users/models.py
+++ users/models.py
@@ -30,3 +30,14 @@ class Office(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=50)
     patients = models.ManyToManyField(Patient)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
