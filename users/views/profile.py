5ad4b26ac926b6a3eb9ff400cac0c69182beb7d1
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-15 18:05:52 +0200

    Added ProfileUpdateForm

--- users/views/profile.py
+++ users/views/profile.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from users.forms import ProfileUpdateForm
 
 
 class Profile(View):
     template_name = 'users/profile.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+        context = {
+            'p_form': p_form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if p_form.is_valid():
+            p_form.save()
+            return redirect('profile')
+
+        context = {
+            'p_form': p_form
+        }
+        return render(request, self.template_name, context)
