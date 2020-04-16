48550b3e2e7059cc2bcc2e2f9f2ecb8fec468af2
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-16 17:54:55 +0200

    Code improvement

--- patient_panel/views/home.py
+++ patient_panel/views/home.py
@@ -1,9 +1,9 @@
-from django.db.models import Count
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from users.models import Office
+from appointments.models import Appointment
 from users.decorators import patient_required
 
 
@@ -11,18 +11,9 @@ from users.decorators import patient_required
 class PatientHome(View):
     template_name = 'patient_panel/patient_home.html'
 
-    def get_queryset(self):
-        queryset = self.request.user.appointments \
-            .select_related('owner') \
-            .annotate(questions_count=Count('owner', distinct=True)) \
-            # .annotate(taken_count=Count('taken_quizzes', distinct=True))
-        return queryset
-
     def get(self, request):
         context = {
             'offices': Office.objects.filter(user__patients__email=self.request.user)[:5],
-            'appointments': self.get_queryset()[:5]
+            'appointments': Appointment.objects.filter(owner=self.request.user)[:5]
         }
         return render(request, 'patient_panel/patient_home.html', context)
-
-
