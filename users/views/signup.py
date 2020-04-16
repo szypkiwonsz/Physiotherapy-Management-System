6ff88a985a6f99a2fee26ac482f27d2aa027c937
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-16 17:39:20 +0200

    Code improvement

--- users/views/signup.py
+++ users/views/signup.py
@@ -1,17 +1,17 @@
 from django.contrib.sites.shortcuts import get_current_site
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from django.views.generic import TemplateView, CreateView
 from users.tokens import account_activation_token
 from django.core.mail import EmailMessage
 from django.contrib import messages
-from django.views import View
-from users.models import Office
 from users.forms import OfficeSignUpForm, PatientSignUpForm
 
 
 class SignUp:
+
     @staticmethod
     def send_email(user, current_site, user_email):
         subject = 'Fizjo-System - Aktywacja Konta'
@@ -28,13 +28,6 @@ class SignUp:
         email.send()
 
     @staticmethod
-    def email_valid(form):
-        email = form.cleaned_data.get('email')
-        confirm_email = form.cleaned_data.get('confirm_email')
-        if email != confirm_email:
-            return False
-
-    @staticmethod
     def user_save(form, is_patient=False, is_office=False):
         user = form.save(commit=False)
         # The account is not active until the user activates it.
@@ -45,55 +38,31 @@ class SignUp:
         return user
 
 
-class Register(View):
+class Register(TemplateView):
     template_name = 'users/signup.html'
 
-    def get(self, request):
-        return render(request, self.template_name)
 
-
-class RegisterPatient(View, SignUp):
+class RegisterPatient(CreateView, SignUp):
     form_class = PatientSignUpForm
-    initial = {'form': 'form'}
     template_name = 'users/signup_patient.html'
 
-    def get(self, request):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = PatientSignUpForm(request.POST)
-        if form.is_valid():
-            user = self.user_save(form, is_patient=True)
-            current_site = get_current_site(request)
-            patient_email = form.cleaned_data.get('email')
-            self.send_email(user, current_site, patient_email)
-            messages.warning(request, 'Potwierdź swoje konto poprzez link wysłany na email.')
-            return redirect('login')
-
-        return render(request, self.template_name, {'form': form})
+    def form_valid(self, form):
+        user = self.user_save(form, is_patient=True)
+        current_site = get_current_site(self.request)
+        patient_email = form.cleaned_data.get('email')
+        self.send_email(user, current_site, patient_email)
+        messages.warning(self.request, 'Potwierdź swoje konto poprzez link wysłany na email.')
+        return redirect('login')
 
 
-class RegisterOffice(View, SignUp):
+class RegisterOffice(CreateView, SignUp):
     form_class = OfficeSignUpForm
-    initial = {'form': 'form'}
     template_name = 'users/signup_office.html'
 
-    def get(self, request):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = OfficeSignUpForm(request.POST)
-        if form.is_valid():
-            user = self.user_save(form, is_office=True)
-            office = Office.objects.create(user=user)
-            office.name = form.cleaned_data.get('name')
-            office.save()
-            current_site = get_current_site(request)
-            office_email = form.cleaned_data.get('email')
-            self.send_email(user, current_site, office_email)
-            messages.warning(request, 'Potwierdź swoje konto poprzez link wysłany na email.')
-            return redirect('login')
-
-        return render(request, self.template_name, {'form': form})
+    def form_valid(self, form):
+        user = self.user_save(form, is_office=True)
+        current_site = get_current_site(self.request)
+        patient_email = form.cleaned_data.get('email')
+        self.send_email(user, current_site, patient_email)
+        messages.warning(self.request, 'Potwierdź swoje konto poprzez link wysłany na email.')
+        return redirect('login')
