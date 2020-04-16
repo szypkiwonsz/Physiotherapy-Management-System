daed90f78c0da0b82fb3ea4e7a3724b7e85cac39
szypkiwonsz <kacpersawicki321@gmail.com>
2020-04-16 18:09:13 +0200

    Code improvement

--- patient_panel/views/appointment.py
+++ patient_panel/views/appointment.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
@@ -11,9 +10,25 @@ from appointments.forms import AppointmentForm
 from users.decorators import patient_required
 
 
+@method_decorator([login_required, patient_required], name='dispatch')
+class AppointmentListView(View):
+    model = Appointment
+    template_name = 'patient_panel/appointments_upcoming.html'
+
+    def get_queryset(self):
+        queryset = self.request.user.appointments.select_related('owner')
+        return queryset
+
+    def get(self, request):
+        context = {
+            'appointments': self.get_queryset(),
+        }
+        return render(request, 'patient_panel/appointments_upcoming.html', context)
+
+
+@method_decorator([login_required, patient_required], name='dispatch')
 class AppointmentCancelView(DeleteView):
     model = Appointment
-    context_object_name = 'appointment'
     template_name = 'patient_panel/appointment_cancel_confirm.html'
     success_url = reverse_lazy('patient-appointment-upcoming')
 
@@ -23,45 +38,16 @@ class AppointmentCancelView(DeleteView):
         return super().delete(request, *args, **kwargs)
 
     def get_queryset(self):
-        name = self.request.user.id
-        return Appointment.objects.filter(owner=name)
-
-
-@method_decorator([login_required, patient_required], name='dispatch')
-class AppointmentListView(View):
-    model = Appointment
-    ordering = ('date', )
-    template_name = 'patient_panel/appointments_upcoming.html'
-
-    def get_queryset(self):
-        queryset = self.request.user.appointments \
-            .select_related('owner') \
-            .annotate(questions_count=Count('owner', distinct=True)) \
-            # .annotate(taken_count=Count('taken_quizzes', distinct=True))
-        return queryset
-
-    def get(self, request):
-        context = {
-            'appointments': self.get_queryset(),
-            # 'form': self.post(request)
-        }
-        return render(request, 'patient_panel/appointments_upcoming.html', context)
+        return Appointment.objects.filter(owner=self.request.user)
 
 
 @method_decorator([login_required, patient_required], name='dispatch')
 class AppointmentUpdateView(UpdateView):
     form_class = AppointmentForm
-    context_object_name = 'appointment'
-    initial = {'form': 'form'}
     template_name = 'patient_panel/appointment_update_form.html'
 
-    # def get_context_data(self, **kwargs):
-    #     kwargs['first_name'] = self.get_object().first_name.annotate()
-    #     return super().get_context_data(**kwargs)
-
     def get_queryset(self):
-        name = self.request.user.id
-        return Appointment.objects.filter(owner=name)
+        return Appointment.objects.filter(owner=self.request.user)
 
     def get_success_url(self):
         return reverse('patient-appointment-change', kwargs={'pk': self.object.pk})
