694abc14efb09074d969655ad7dab814929be286
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-01 20:30:02 +0200

    Commit

--- medical_history/views.py
+++ medical_history/views.py
@@ -13,26 +13,16 @@ from .forms import MedicalHistoryForm
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
 
+@method_decorator([login_required, office_required], name='dispatch')
 class MakeMedicalHistory(CreateView):
     form_class = MedicalHistoryForm
-    template_name = 'medical_history/add_medical_history.html'
+    template_name = 'medical_history/medical_history_add_form.html'
 
     def form_valid(self, form):
         post = form.save(commit=False)
         post.owner_id = self.request.user.id
         post.save()
-        return redirect('office-patients')
-
-
-class MedicalHistoryListView(View):
-    model = MedicalHistory
-    template_name = 'office_panel/office_medical_history.html'
-
-    def get(self, request):
-        context = {
-            'medical_histories': MedicalHistory.objects.filter(owner=self.request.user.id),
-        }
-        return render(request, 'office_panel/office_medical_history.html', context)
+        return redirect('office-medical-history')
 
 
 @method_decorator([login_required, office_required], name='dispatch')
@@ -49,7 +39,7 @@ class MedicalHistoryDetailView(DetailView):
 
 class MedicalHistoryUpdateView(UpdateView):
     form_class = MedicalHistoryForm
-    template_name = 'office_panel/medical_history_update_form.html'
+    template_name = 'medical_history/medical_history_update_form.html'
 
     def get_queryset(self):
         return MedicalHistory.objects.filter(owner=self.request.user.id)
@@ -58,13 +48,24 @@ class MedicalHistoryUpdateView(UpdateView):
         return reverse('office-medical-history-change', kwargs={'pk': self.object.pk})
 
 
+@method_decorator([login_required, office_required], name='dispatch')
+class MedicalHistoryDetailView(DetailView):
+    form_class = MedicalHistoryForm
+    template_name = 'medical_history/medical_history_detail_form.html'
+
+    def get_queryset(self):
+        return MedicalHistory.objects.all()
+
+    def get_success_url(self):
+        return reverse('office-patient-change', kwargs={'pk': self.object.pk})
+
+
 class MedicalHistoryDeleteView(DeleteView):
     form_class = MedicalHistoryForm
-    template_name = 'office_panel/medical_history_delete_confirm.html'
-    success_url = reverse_lazy('office-home')
+    template_name = 'medical_history/medical_history_delete_confirm.html'
+    success_url = reverse_lazy('office-medical-history')
 
     def delete(self, request, *args, **kwargs):
-        appointment = self.get_object()
         messages.success(request, f'Wizyta została poprawnie usunięta.')
         return super().delete(request, *args, **kwargs)
 
