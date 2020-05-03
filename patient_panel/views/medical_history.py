da8e596c3f24aa6f0dc0ceaa37da42bb1fe3b92d
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-03 13:16:05 +0200

    Commit

---
+++ patient_panel/views/medical_history.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from medical_history.models import MedicalHistory
+from users.decorators import login_required, patient_required
+
+
+@method_decorator([login_required, patient_required], name='dispatch')
+class MedicalHistoryListView(View):
+    model = MedicalHistory
+    template_name = 'patient_panel/patient_medical_history.html'
+
+    def get_queryset(self):
+        queryset = MedicalHistory.objects.filter(patient__email=self.request.user)
+        return queryset
+
+    def get(self, request):
+        context = {
+            'medical_histories': self.get_queryset(),
+        }
+        return render(request, 'patient_panel/patient_medical_history.html', context)
