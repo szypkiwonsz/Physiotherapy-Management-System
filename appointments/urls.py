254d39bd7c28a402d1870777fd80b8c33b86a174
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-03 14:47:37 +0200

    Commit

--- appointments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('select/', views.SelectOffice.as_view(), name='appointments-select'),
-    path('<int:pk>/', views.MakeAppointment.as_view(), name='appointments-make-appointment'),
-]
