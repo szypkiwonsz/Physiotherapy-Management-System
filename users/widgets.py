4bd8f4f28a377081e928b355db058a7c7fd757be
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-03 12:10:38 +0200

    Comment added

--- users/widgets.py
+++ users/widgets.py
@@ -1,6 +1,7 @@
 from django.forms import ClearableFileInput
 
 
+# Profile picture label text.
 class MyClearableFileInput(ClearableFileInput):
     initial_text = 'Aktualne'
     input_text = 'Zmień tutaj'
