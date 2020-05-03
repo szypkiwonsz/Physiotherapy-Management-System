1dcdda1a19c121d7633312129f4b1406237bf4c2
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-03 12:11:03 +0200

    Signup names changed

--- users/urls.py
+++ users/urls.py
@@ -3,18 +3,18 @@ from users.views import signup, login, activate_account, password, profile
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('profile/office/', profile.OfficeProfile.as_view(), name='profile-office'),
-    path('profile/patient/', profile.PatientProfile.as_view(), name='profile-patient'),
     path('login/', login.Login.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('signup/', signup.Register.as_view(), name='signup'),
-    path('signup/patient/', signup.RegisterPatient.as_view(), name='patient-signup'),
-    path('signup/office/', signup.RegisterOffice.as_view(), name='office-signup'),
+    path('signup/patient/', signup.RegisterPatient.as_view(), name='signup-patient'),
+    path('signup/office/', signup.RegisterOffice.as_view(), name='signup-office'),
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             activate_account.activate, name='activate'),
+    path('profile/office/', profile.OfficeProfile.as_view(), name='profile-office'),
+    path('profile/patient/', profile.PatientProfile.as_view(), name='profile-patient'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             password.NewPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    ]
+]
