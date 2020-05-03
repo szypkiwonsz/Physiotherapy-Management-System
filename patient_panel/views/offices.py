f442d5219153a75ac4647841c826895631b7b166
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-03 13:15:30 +0200

    Commit

---
+++ patient_panel/views/offices.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from users.models import Office
+from users.decorators import login_required, patient_required
+
+
+@method_decorator([login_required, patient_required], name='dispatch')
+class OfficesListView(View):
+    model = Office
+    template_name = 'patient_panel/patient_office.html'
+
+    def get_queryset(self):
+        queryset = Office.objects.filter(user__patients__email=self.request.user)
+        return queryset
+
+    def get(self, request):
+        context = {
+            'offices': self.get_queryset(),
+        }
+        return render(request, 'patient_panel/patient_office.html', context)
