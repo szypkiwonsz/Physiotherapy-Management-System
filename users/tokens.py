a2f7ab3f7d1a5d51f8e7075de29942b5d2550f1f
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-03 12:16:22 +0200

    Import changed

--- users/tokens.py
+++ users/tokens.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
 import six
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 # Token generator for user account activation.
