82115b91d67b8c0e61ad0fa0e55bf5a2c0f0614e
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-01 21:52:23 +0200

    Email changes, added date_input_formats

--- Physiotherapy_Management_System/settings.py
+++ Physiotherapy_Management_System/settings.py
@@ -116,7 +116,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
@@ -144,5 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kacpersawicki321@gmail.com'
-EMAIL_HOST_PASSWORD = 'csrryfawrmkqkrfz'
+EMAIL_HOST_PASSWORD = 'aqoywqbvjjcnlyxq'
 EMAIL_PORT = 587
+
+DATE_INPUT_FORMATS = ['%d.%m.%Y %H:%M']
+DATE_FORMAT = ['%d.%m.%Y %H:%M']
