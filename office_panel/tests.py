a5bf2907b0a4e75b3e28197cc80e302c4c2a5756
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-08 18:20:33 +0200

    Deleted bad test

--- office_panel/tests.py
+++ office_panel/tests.py
@@ -1,10 +1,4 @@
-from django.urls import reverse
 from django.test import TestCase
 
 
 # Create your tests here.
-class OfficeHomeTests(TestCase):
-    def test_office_home_view_status_code(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
