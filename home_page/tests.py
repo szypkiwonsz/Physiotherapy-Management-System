1f6665452c920652fa0efe277dd88e00cd595259
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-08 18:24:15 +0200

    Added class HelpTests

--- home_page/tests.py
+++ home_page/tests.py
@@ -9,3 +9,10 @@ class HomeTests(TestCase):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
+
+
+class HelpTests(TestCase):
+    def test_help_view_status_code(self):
+        url = reverse('help')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
