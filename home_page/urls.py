3e37d570d4a3f36e96c3cd0dd565662cc6329c19
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-09 22:21:48 +0200

    Code improvement

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomeView, Offices, HelpView
+from home_page.views import HomeView, OfficesView, HelpView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('help/', HelpView.as_view(), name='help'),
-    path('offices/', Offices.as_view(), name='offices')
+    path('offices/', OfficesView.as_view(), name='offices')
     ]
