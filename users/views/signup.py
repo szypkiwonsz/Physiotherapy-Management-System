020588046494e35e13bdff95ecae5b99b5332d82
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-15 23:04:47 +0200

    Function moved

--- users/views/signup.py
+++ users/views/signup.py
@@ -1,32 +1,13 @@
 from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import redirect
-from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
 from django.views.generic import TemplateView, CreateView
-from django.core.mail import EmailMessage
 from django.contrib import messages
 from users.models import Office, UserPatient
-from users.tokens import account_activation_token
 from users.forms import OfficeSignUpForm, PatientSignUpForm
+from utils.send_email import activation_email
 
 
 class SignUp:
-
-    @staticmethod
-    def send_email(user, current_site, user_email):
-        subject = 'Fizjo-System - Aktywacja Konta'
-        message = render_to_string('users/activate_email.html', {
-            'user': user,
-            'domain': current_site.domain,
-            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            'token': account_activation_token.make_token(user),
-        })
-        to_email = user_email
-        email = EmailMessage(
-            subject, message, to=[to_email]
-        )
-        email.send()
 
     @staticmethod
     def user_save(form, is_patient=False, is_office=False):
@@ -54,7 +35,7 @@ class RegisterPatient(CreateView, SignUp):
         user_patient.save()
         current_site = get_current_site(self.request)
         patient_email = form.cleaned_data.get('email')
-        self.send_email(user, current_site, patient_email)
+        activation_email(user, current_site, patient_email)
         messages.warning(self.request, 'Potwierdź swoje konto poprzez link wysłany na email.')
         return redirect('login')
 
@@ -72,6 +53,6 @@ class RegisterOffice(CreateView, SignUp):
         office.save()
         current_site = get_current_site(self.request)
         office_email = form.cleaned_data.get('email')
-        self.send_email(user, current_site, office_email)
+        activation_email(user, current_site, office_email)
         messages.warning(self.request, 'Potwierdź swoje konto poprzez link wysłany na email.')
         return redirect('login')
