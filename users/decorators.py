8380bc5faa05043c58da0cfdeb6c986573abdbe6
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-19 00:29:55 +0200

    Redirect url changed

--- users/decorators.py
+++ users/decorators.py
@@ -7,7 +7,7 @@ def patient_required(function):
     def _function(request, *args, **kwargs):
         if not request.user.is_patient:
             messages.warning(request, 'Aby mieć dostęp do tej sekcji, zaloguj się jako pacjent.')
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+            return redirect('login')
         return function(request, *args, **kwargs)
     return _function
 
@@ -16,7 +16,7 @@ def office_required(function):
     def _function(request, *args, **kwargs):
         if not request.user.is_office:
             messages.warning(request, 'Aby mieć dostęp do tej sekcji, zaloguj się jako gabinet.')
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+            return redirect('login')
         return function(request, *args, **kwargs)
     return _function
 
