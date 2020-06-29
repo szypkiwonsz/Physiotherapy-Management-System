3ec9ff55a376623e14efdab2266d122765ac63d3
szypkiwonsz <kacpersawicki321@gmail.com>
2020-06-29 19:53:56 +0200

    Tests added

--- appointments/tests/test_views.py
+++ appointments/tests/test_views.py
@@ -20,22 +20,12 @@ class TestViews(TestCase):
             'office', 'office@gmail.com', 'officepassword', is_office=True
         )
         self.appointment_office1 = Office.objects.create(
-            user=self.office1,
-            name='Name',
-            address='Address',
+            user=self.patient1,
+            name='name',
+            address='address',
             city='City',
             phone_number='000000000',
             website='www.website.com'
-        )
-        self.appointment1 = Appointment.objects.create(
-            owner=self.patient1,
-            office=self.appointment_office1,
-            date=datetime(2012, 1, 13, 16, 00, 00),
-            name='Name',
-            date_selected=datetime(2012, 1, 13, 23, 51, 34),
-            phone_number=000000000,
-            confirmed=False,
-            choice='Konsultacja'
         )
 
     def test_select_office_GET_not_logged_in(self):
@@ -58,14 +48,46 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 302)
         self.assertTemplateNotUsed(response, 'appointment/appointment_select_office.html')
 
+    def test_make_appointment_create_GET_not_logged_in(self):
+        response = self.client.get(self.make_appointment_url)
+
+        self.assertEquals(response.status_code, 302)
+        self.assertTemplateNotUsed(response, 'appointment/appointment_make_form.html')
+
+    def test_make_appointment_create_GET_logged_as_patient(self):
+        self.client.login(username='patient@gmail.com', password='patientpassword')
+        response = self.client.get(self.make_appointment_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'appointment/appointment_make_form.html')
+
+    def test_make_appointment_create_GET_logged_as_office(self):
+        self.client.login(username='office@gmail.com', password='officepassword')
+        response = self.client.get(self.make_appointment_url)
+
+        self.assertEquals(response.status_code, 302)
+        self.assertTemplateNotUsed(response, 'appointment/appointment_make_form.html')
+
     def test_make_appointment_create_POST(self):
-        url = reverse('appointments-make-appointment', args=[2])
+        url = reverse('appointments-make-appointment', args=[1])
+        Appointment.objects.create(
+            owner=self.office1,
+            office=self.appointment_office1,
+            date=datetime(2012, 1, 13, 16, 00, 00),
+            name='name',
+            date_selected=datetime(2012, 1, 13, 23, 51, 34),
+            phone_number='000000000',
+            confirmed=False,
+            choice='Konsultacja'
+        )
         self.client.login(username='patient@gmail.com', password='patientpassword')
         response = self.client.post(url, {
             'date': '17.02.1998 17:00',
-            'name': 'Name',
-            'phone_number': 000000000,
+            'name': 'name',
+            'phone_number': '000000000',
             'choice': 'Konsultacja'
         })
         appointment2 = Appointment.objects.get(id=2)
         self.assertEquals(appointment2.date, datetime(1998, 2, 17, 17, 00, 00, tzinfo=UTC))
+        self.assertEquals(appointment2.office_id, 1)
+        self.assertEquals(appointment2.confirmed, False)
