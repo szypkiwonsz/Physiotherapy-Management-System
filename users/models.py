2ae3935d5ac75a47033012df6f953c24ef6f1369
szypkiwonsz <kacpersawicki321@gmail.com>
2020-07-21 22:18:46 +0200

    Patient model transfer

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from PIL import Image
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
+from django.db import models
 from django.utils.translation import gettext as _
 
 
@@ -11,8 +10,8 @@ class User(AbstractUser):
 
     username = models.CharField(max_length=40, unique=False, default='')
     email = models.EmailField(unique=True, error_messages={
-            'unique': _("Użytkownik z takim adresem email już istnieje."),
-        },)
+        'unique': _("Użytkownik z takim adresem email już istnieje."),
+    }, )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -21,25 +20,6 @@ class User(AbstractUser):
 class UserPatient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     phone_number = models.CharField(max_length=9)
-
-
-class Patient(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patients')
-    first_name = models.CharField(max_length=20, unique=False, default='')
-    last_name = models.CharField(max_length=40, unique=False, default='')
-    email = models.EmailField(unique=True, default='')
-    address = models.CharField(max_length=50)
-    pesel = models.CharField(max_length=11)
-    phone_number = models.CharField(max_length=9)
-    date_selected = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-    def save(self, *args, **kwargs):
-        self.first_name = self.first_name.capitalize()
-        self.last_name = self.last_name.capitalize()
-        super(Patient, self).save(*args, **kwargs)
 
 
 class Office(models.Model):
