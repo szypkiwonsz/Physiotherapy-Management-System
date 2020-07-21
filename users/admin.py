7b120ff8d6be3332815b219f90b7c88ef9c6c7ce
szypkiwonsz <kacpersawicki321@gmail.com>
2020-07-21 20:23:38 +0200

    Imports optimized

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User, Patient, Office, Profile
+
+from office_panel.models import Patient
+from .models import User, Office, Profile
 
 admin.site.register(User)
 admin.site.register(Patient)
