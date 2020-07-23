b859ba2bf76677f08b828b2d4a4d469f06d4de01
szypkiwonsz <kacpersawicki321@gmail.com>
2020-07-23 20:46:26 +0200

    Deleted unused function

--- appointments/models.py
+++ appointments/models.py
@@ -1,14 +1,8 @@
-import random
-
 from django.db import models
 from django.utils import timezone
 
 from office_panel.models import Patient
 from users.models import User, Office
-
-
-def random_string():
-    return str(random.randint(10000, 99999))
 
 
 # Create your models here.
@@ -19,7 +13,6 @@ class Appointment(models.Model):
     name = models.CharField('Enter your first name:', max_length=120)
     date_selected = models.DateTimeField(default=timezone.now)
     phone_number = models.CharField(max_length=9)
-    # Argument 'on_delete=models.CASCADE' - when User will be deleted, his post will be deleted too.
     confirmed = models.BooleanField(default=False)
     choice = models.CharField(max_length=120)
 
