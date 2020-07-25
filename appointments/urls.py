95e84968fc3f6debaa596388b83080a94a109c23
szypkiwonsz <kacpersawicki321@gmail.com>
2020-07-25 14:18:20 +0200

    Added urls for office and patient user

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from . import views
+from appointments.views import patient, office
 
 urlpatterns = [
-    path('select/', views.SelectOffice.as_view(), name='appointments-select'),
-    path('<int:pk>/', views.MakeAppointment.as_view(), name='appointments-make-appointment'),
+    path('select/', patient.SelectOffice.as_view(), name='appointments-select'),
+    path('old/', patient.OldAppointmentListView.as_view(), name='patient-appointment-old'),
+    path('upcoming/', patient.AppointmentListView.as_view(), name='patient-appointment-upcoming'),
+    path('<int:pk>/change/', patient.AppointmentUpdateView.as_view(), name='patient-appointment-change'),
+    path('<int:pk>/cancel/', patient.AppointmentCancelView.as_view(), name='patient-appointment-cancel'),
+    path('<int:pk>/make/', patient.MakeAppointment.as_view(), name='appointments-make-appointment'),
+
+    path('', office.AppointmentListView.as_view(), name='office-appointments'),
+    path('<int:pk>/update', office.AppointmentUpdateView.as_view(), name='office-appointment-change'),
+    path('<int:pk>/delete/', office.AppointmentDeleteView.as_view(), name='office-appointment-delete'),
 ]
