7f63ec3699f7daae70476ee40d576b94d72d53f4
szypkiwonsz <kacpersawicki321@gmail.com>
2020-07-26 15:24:31 +0200

    Tests changed

--- office_panel/tests/test_urls.py
+++ office_panel/tests/test_urls.py
@@ -1,34 +1,12 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from office_panel.views import home, patient, medical_history
+from office_panel.views import home, patient
 
 
 class TestOfficeHomeUrls(SimpleTestCase):
     def test_patient_list_url_resolves(self):
         url = reverse('office-home')
         self.assertEquals(resolve(url).func.view_class, home.OfficePanelView)
-
-
-class TestOfficeMedicalHistoryUrls(SimpleTestCase):
-    def test_medical_history_list_url_resolves(self):
-        url = reverse('office-medical-history')
-        self.assertEquals(resolve(url).func.view_class, medical_history.MedicalHistoryListView)
-
-    def test_medical_history_add_url_resolves(self):
-        url = reverse('office-make-medical-history')
-        self.assertEquals(resolve(url).func.view_class, medical_history.MakeMedicalHistory)
-
-    def test_medical_history_detail_url_resolves(self):
-        url = reverse('office-medical-history-detail', args=[1])
-        self.assertEquals(resolve(url).func.view_class, medical_history.MedicalHistoryDetailView)
-
-    def test_medical_history_update_url_resolves(self):
-        url = reverse('office-medical-history-change', args=[1])
-        self.assertEquals(resolve(url).func.view_class, medical_history.MedicalHistoryUpdateView)
-
-    def test_medical_history_delete_url_resolves(self):
-        url = reverse('office-medical-history-delete', args=[1])
-        self.assertEquals(resolve(url).func.view_class, medical_history.MedicalHistoryDeleteView)
 
 
 class TestOfficePatientUrls(SimpleTestCase):
--- patient_panel/tests/test_urls.py
+++ patient_panel/tests/test_urls.py
@@ -1,18 +1,12 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from patient_panel.views import home, offices, medical_history
+from patient_panel.views import home, offices
 
 
 class TestPatientHomeUrls(SimpleTestCase):
     def test_home_url_resolves(self):
         url = reverse('patient-home')
         self.assertEquals(resolve(url).func.view_class, home.PatientHome)
-
-
-class TestPatientMedicalHistoryUrls(SimpleTestCase):
-    def test_medical_history_list_url_resolves(self):
-        url = reverse('patient-medical-history')
-        self.assertEquals(resolve(url).func.view_class, medical_history.MedicalHistoryListView)
 
 
 class TestPatientOfficesUrls(SimpleTestCase):
