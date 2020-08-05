00abc0147bf2fb322d4b0e402f7ff234298056e2
szypkiwonsz <kacpersawicki321@gmail.com>
2020-08-05 23:54:22 +0200

    Imports optimized

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
+from django.urls import path, re_path
+
 from users.views import signup, login, activate_account, password, profile
 
 urlpatterns = [
