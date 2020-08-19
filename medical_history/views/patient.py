39c3d3245bcf563cc1dc8bf55fd6e4117561b05e
szypkiwonsz <kacpersawicki321@gmail.com>
2020-08-19 17:17:17 +0200

    Add pagination and filtering

--- medical_history/views/patient.py
+++ medical_history/views/patient.py
@@ -1,8 +1,12 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 
 from medical_history.models import MedicalHistory
 from users.decorators import login_required, patient_required
+from utils.paginate import paginate
 
 
 @method_decorator([login_required, patient_required], name='dispatch')
@@ -14,3 +18,30 @@ class MedicalHistoryListView(ListView):
     def get_queryset(self):
         queryset = MedicalHistory.objects.filter(patient__email=self.request.user).order_by('-date_selected')
         return queryset
+
+    def get(self, request, **kwargs):
+        url_without_parameters = str(request.get_full_path()).split('?')[0]
+        url_parameter_q = request.GET.get('q')
+        if url_parameter_q:
+            ctx = {
+                'medical_histories': self.get_queryset().filter(owner__office__name__icontains=url_parameter_q),
+            }
+        else:
+            ctx = {
+                'medical_histories': self.get_queryset(),
+            }
+            paginated_medical_histories = paginate(request, ctx['medical_histories'], 2)
+
+            ctx = {
+                'medical_histories': paginated_medical_histories,
+                'endpoint': url_without_parameters
+            }
+
+        if request.is_ajax():
+            html = render_to_string(
+                template_name='medical_history/patient/medical_history_results_partial.html',
+                context=ctx
+            )
+            data_dict = {"html_from_view": html}
+            return JsonResponse(data=data_dict, safe=False)
+        return render(request, self.template_name, ctx)
