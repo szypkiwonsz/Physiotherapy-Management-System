199066c8d5c3a81850e42b559f6ac29c5a2e7c98
szypkiwonsz <kacpersawicki321@gmail.com>
2020-08-20 18:57:08 +0200

    Add validation

--- office_panel/models.py
+++ office_panel/models.py
@@ -1,17 +1,30 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext as _
 
 from users.models import User
+
+numeric_phone_number = RegexValidator('^[0-9]*$', 'Jako numer telefonu, możesz podać jedynie cyfry.')
+numeric_pesel = RegexValidator('^[0-9]*$', 'Jako pesel, możesz podać jedynie cyfry.')
+alphanumeric_first_name = RegexValidator('^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]*$', 'Imię pacjenta nie może zawierać cyfr.')
+alphanumeric_last_name = RegexValidator('^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]*$', 'Nazwisko pacjenta nie może zawierać cyfr.')
 
 
 class Patient(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patients')
-    first_name = models.CharField(max_length=20, unique=False, default='')
-    last_name = models.CharField(max_length=40, unique=False, default='')
-    email = models.EmailField(unique=True, default='')
+    first_name = models.CharField(max_length=20, unique=False, default='', validators=[alphanumeric_first_name])
+    last_name = models.CharField(max_length=40, unique=False, default='', validators=[alphanumeric_last_name])
+    email = models.EmailField(unique=True, default='', error_messages={
+        'unique': _("Pacjent z takim adresem email już istnieje.")
+    })
     address = models.CharField(max_length=50)
-    pesel = models.CharField(max_length=11)
-    phone_number = models.CharField(max_length=9)
+    pesel = models.CharField(max_length=11, validators=[numeric_pesel], unique=True, error_messages={
+        'unique': _("Pacjent z takim numerem pesel już istnieje.")
+    })
+    phone_number = models.CharField(max_length=9, validators=[numeric_phone_number], unique=True, error_messages={
+        'unique': _("Pacjent z takim numerem telefonu już istnieje.")
+    })
     date_selected = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
