6c51d8f12b5f9b74f93eaf61d7dbb34cf6450fa9
szypkiwonsz <kacpersawicki321@gmail.com>
2020-08-28 16:33:20 +0200

    Code improvement
    - fix url names,
    - set app_name

--- patient_panel/urls.py
+++ patient_panel/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
+
 from patient_panel.views import home, offices
 
+app_name = 'patient_panel'
 urlpatterns = [
-    path('', home.PatientHome.as_view(), name='patient-home'),
-    path('offices/', offices.OfficesListView.as_view(), name='patient-offices'),
+    path('', home.PatientHome.as_view(), name='home'),
+    path('offices/', offices.OfficesListView.as_view(), name='offices'),
     path('medical_history/', include('medical_history.urls.patient')),
     path('appointments/', include('appointments.urls.patient')),
 ]
