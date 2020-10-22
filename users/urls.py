c88090e5b601e248ea0d09d283cf068ca99acd47
szypkiwonsz <kacpersawicki321@gmail.com>
2020-10-22 20:52:38 +0200

    Improve urlpatterns for activating account and resetting password

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,13 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path, re_path, include
+from django.urls import path, include
 
 from users.views import signup, activate_account, password, profile
 
 app_name = 'users'
 urlpatterns = [
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            activate_account.activate, name='activate'),
-    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            password.NewPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('activate/<slug:uidb64>/<slug:token>/',
+         activate_account.activate, name='activate'),
+    path('reset/<slug:uidb64>/<slug:token>/',
+         password.NewPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('signup/', include([
         path('', signup.Register.as_view(), name='signup'),
         path('patient/', signup.RegisterPatient.as_view(), name='patient_signup'),
