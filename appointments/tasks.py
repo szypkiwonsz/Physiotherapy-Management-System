2923ed3a3be7b157d2b3a5476be4d41adcbdb47e
szypkiwonsz <kacpersawicki321@gmail.com>
2020-10-26 16:14:36 +0100

    Reformat code

--- appointments/tasks.py
+++ appointments/tasks.py
@@ -1,19 +1,13 @@
-from django.core.mail import EmailMessage
 from celery import shared_task
-from utils.date_time import DateTime
+from django.core.mail import EmailMessage
 
 
 @shared_task
-def appointment_confirmation_office(patient_name, date, time, office_email):
-    hour = time[0]
-    minute = time[1]
-    day = date[2]
-    month = date[1]
-    year = date[0]
+def appointment_confirmation_email_office(patient_name, date, time, office_email):
+    date = '.'.join(date)
+    time = ':'.join(time[:2])
     subject = 'Fizjo-System - Nowa Wizyta'
-    message = f'\nPacjent {patient_name} umówił wizytę w twoim gabinecie na godzinę {hour}:' \
-              f'{minute} dnia {day}.{month}.' \
-              f'{year}.\n\n' \
+    message = f'\nPacjent {patient_name} umówił wizytę w twoim gabinecie na godzinę {time} dnia {date}.\n\n' \
               'Potwierdź wizytę pacjenta logując się do panelu swojego gabinetu.\n\n' \
               'Fizjo-System'
     email = EmailMessage(subject, message, to=[office_email])
@@ -21,17 +15,13 @@ def appointment_confirmation_office(patient_name, date, time, office_email):
 
 
 @shared_task
-def appointment_confirmation_patient(patient_name, office_name, date, time, patient_email):
-    hour = time[0]
-    minute = time[1]
-    day = date[2]
-    month = date[1]
-    year = date[0]
+def appointment_confirmation_email_patient(patient_name, office_name, date, time, patient_email):
+    date = '.'.join(date)
+    time = ':'.join(time[:2])
     subject = 'Fizjo-System - Umówiono Wizytę'
     message = f'\nWitaj {patient_name},\n\n' \
-              f'Twoja wizyta umówiona na godzinę {hour}:' \
-              f'{minute} dnia {day}.{DateTime.add_zero(month)}.' \
-              f'{year} w gabinecie {office_name} oczekuje na potwierdzenie.\n' \
+              f'Twoja wizyta umówiona na godzinę {time} dnia {date} w gabinecie {office_name} ' \
+              f'oczekuje na potwierdzenie.\n' \
               f'Potwierdzenie wizyty ze strony gabinetu otrzymasz w kolejnym mailu.\n\n' \
               'Jeśli chcesz odwołać wizytę, skorzystaj z opcji odwołaj znajdującej się w panelu pacjenta.\n\n' \
               'Fizjo-System'
