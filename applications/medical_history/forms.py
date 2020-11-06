5ab11e26c4bdb63027c702079f14081e698b19a7
szypkiwonsz <kacpersawicki321@gmail.com>
2020-11-06 22:11:40 +0100

    Fix bug with displaying all visits and all patients

--- applications/medical_history/forms.py
+++ applications/medical_history/forms.py
@@ -2,13 +2,21 @@ from django import forms
 
 from applications.appointments.models import Appointment
 from applications.medical_history.models import MedicalHistory
+from applications.office_panel.models import Patient
 
 
 class MedicalHistoryForm(forms.ModelForm):
-    appointment = forms.ModelChoiceField(queryset=Appointment.objects.all(), required=False)
 
     def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
         super(MedicalHistoryForm, self).__init__(*args, **kwargs)
+
+        self.fields['appointment'] = forms.ModelChoiceField(
+            queryset=Appointment.objects.filter(office__user=self.user), required=False
+        )
+        self.fields['patient'] = forms.ModelChoiceField(
+            queryset=Patient.objects.filter(owner=self.user), required=False
+        )
 
         label = ['Pacjent', 'Wizyta', 'Opis', 'Zalecenia']
         for i, field_name in enumerate(['patient', 'appointment', 'description', 'recommendations']):
