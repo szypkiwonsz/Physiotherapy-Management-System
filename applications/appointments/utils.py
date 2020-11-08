2347663b76febad13c734ed478ceae3a2e06e4ed
szypkiwonsz <kacpersawicki321@gmail.com>
2020-11-08 22:09:06 +0100

    Remove function
    Moved function to utils.add_zero.py

--- applications/appointments/utils.py
+++ applications/appointments/utils.py
@@ -9,16 +9,3 @@ def database_old_datetime_format_to_new(datetime_string):
     """
     new_datetime = datetime.strptime(datetime_string.split('+')[0], '%Y-%m-%d %H:%M:%S')
     return datetime.strftime(new_datetime, '%d.%m.%Y %H:%M')
-
-
-def add_zero(number):
-    """
-    # Add zero before number if number is smaller than 10.
-    :param number: <string> -> number
-    :return: <string> -> number
-    """
-    if int(number) < 10:
-        number = '0' + str(number)
-    else:
-        pass
-    return number
