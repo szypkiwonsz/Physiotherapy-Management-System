fb918e12b5c3b8e89605634abeae608b5a9bd3cd
szypkiwonsz <kacpersawicki321@gmail.com>
2020-11-14 21:29:11 +0100

    Move methods due to code reformatting

---
+++ applications/office_panel/utils.py
@@ -0,0 +1,13 @@
+import calendar
+import datetime
+
+
+def get_number_of_days_in_month(year, month):
+    num_days = calendar.monthrange(year, month)[1]
+    days = [datetime.date(year, month, day).day for day in range(1, num_days + 1)]
+    return days
+
+
+def get_hours_in_day(hour_open, hour_close):
+    hours = [f'{i}:00' for i in range(hour_open, hour_close)]
+    return hours
