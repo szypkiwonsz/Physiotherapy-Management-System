97901166748df076a9f4e4a4a6d49fbeaa7f7fa5
szypkiwonsz <kacpersawicki321@gmail.com>
2020-11-18 22:03:15 +0100

    Register OfficeDay model in admin panel

--- applications/users/admin.py
+++ applications/users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from applications.users.models import User, Office, Profile, UserPatient
+from applications.users.models import User, Office, Profile, UserPatient, OfficeDay
 
 admin.site.register(User)
 admin.site.register(UserPatient)
 admin.site.register(Office)
 admin.site.register(Profile)
+admin.site.register(OfficeDay)
