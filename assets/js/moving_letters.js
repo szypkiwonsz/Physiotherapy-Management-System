4009bc336dfacea04adea55a3c4acbfc1c2ec840
szypkiwonsz <kacpersawicki321@gmail.com>
2020-11-27 20:26:18 +0100

    Code reformat

--- assets/js/moving_letters.js
+++ assets/js/moving_letters.js
@@ -1,4 +1,4 @@
-// Wrap every letter in a span
+// Wrap every letter in a span.
 var textWrapper = document.querySelector('.title');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
