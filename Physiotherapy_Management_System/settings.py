2f51050212e38b8d1c5c97e57713de4578013407
szypkiwonsz <kacpersawicki321@gmail.com>
2020-11-28 17:31:48 +0100

    Fix typo

--- Physiotherapy_Management_System/settings.py
+++ Physiotherapy_Management_System/settings.py
@@ -170,8 +170,8 @@ else:
 
 # Celery settings
 
-CELERY_BROKER_URL = os.environ.get('CELERY_URL')
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_URL')
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
