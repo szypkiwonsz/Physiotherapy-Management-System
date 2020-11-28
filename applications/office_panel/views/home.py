1e728884a4a4c9c4520ed796141e9c7f24774876
szypkiwonsz <kacpersawicki321@gmail.com>
2020-11-28 12:09:08 +0100

    Reformat code
    Move code to separate method.
    Style improvement.

--- applications/office_panel/views/home.py
+++ applications/office_panel/views/home.py
@@ -17,6 +17,20 @@ class OfficePanelView(View):
     model = Patient
     template_name = 'office_panel/home.html'
 
+    @staticmethod
+    def get_next_available_dates(all_dates, dates_taken, tomorrow):
+        """Returns a list of upcoming appointments dates that can be arranged (from tomorrow)."""
+        result_dates = []
+        for dates in all_dates:
+            list_of_dates = []
+            for date in dates:
+                # display dates only from tomorrow and if date is not taken by another patient
+                if datetime.datetime.strptime(date, '%d.%m.%Y %H:%M') > tomorrow and date not in dates_taken:
+                    list_of_dates.append(date)
+            if list_of_dates:
+                result_dates.append(list_of_dates)
+        return result_dates
+
     def get(self, request):
         now = datetime.datetime.now()
         yesterday = datetime.datetime(now.year, now.month, now.day - 1)
@@ -24,26 +38,17 @@ class OfficePanelView(View):
 
         days = get_number_of_days_in_month(now.year, now.month)
         dates = get_dates_in_month(request=request, days_in_month=days, month=now.month, year=now.year)
-        taken_dates = Appointment.objects.filter(
-            date__gte=yesterday, office__user=request.user
-        )
+        appointments = Appointment.objects.filter(date__gte=yesterday, office__user=request.user)
         all_dates = dates
-        taken_dates = [str(x.date.strftime('%d.%m.%Y %H:%M')) for x in taken_dates]
-        final_dates = []
-        for dates in all_dates:
-            list_of_dates = []
-            for date in dates:
-                if datetime.datetime.strptime(date, '%d.%m.%Y %H:%M') > tomorrow and date not in taken_dates:
-                    list_of_dates.append(date)
-            if list_of_dates:
-                final_dates.append(list_of_dates)
+        dates_taken = [str(x.date.strftime('%d.%m.%Y %H:%M')) for x in appointments]
+        result_dates = self.get_next_available_dates(all_dates, dates_taken, tomorrow)
 
-        context = {
+        ctx = {
             'patients': Patient.objects.filter(owner=self.request.user.id).order_by('-date_selected')[:5],
-            'dates': final_dates,
+            'dates': result_dates,
             'appointments': Appointment.objects.filter(office=self.request.user.id).order_by('date')[:5],
             'medical_histories': MedicalHistory.objects.filter(owner=self.request.user.id).order_by(
                 '-date_selected')[:5],
             'office_id': self.request.user.pk
         }
-        return render(request, self.template_name, context)
+        return render(request, self.template_name, ctx)
