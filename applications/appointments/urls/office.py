2283fbd66912ef7803568780a6b6a2f24bdb06b7
szypkiwonsz <kacpersawicki321@gmail.com>
2020-12-17 18:47:26 +0100

    Move timetable url path to appointments urls

--- applications/appointments/urls/office.py
+++ applications/appointments/urls/office.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from applications.appointments.views import office
+from applications.appointments.views import office, timetable
+from applications.users.decorators import office_required, login_required
 
 app_name = 'appointments'
 urlpatterns = [
     path('', office.AppointmentListView.as_view(), name='list'),
+    path('timetable/', login_required(office_required(timetable.TimetableView.as_view())), name='timetable'),
     path('<int:pk>/', include([
         path('update/', office.AppointmentUpdateView.as_view(), name='update'),
         path('delete/', office.AppointmentDeleteView.as_view(), name='delete'),
--- applications/appointments/urls/patient.py
+++ applications/appointments/urls/patient.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
 from applications.appointments.views import patient
+from applications.appointments.views import timetable
+from applications.users.decorators import patient_required, login_required
 
 app_name = 'appointments'
 urlpatterns = [
@@ -8,6 +10,7 @@ urlpatterns = [
     path('old/', patient.OldAppointmentListView.as_view(), name='old'),
     path('upcoming/', patient.AppointmentListView.as_view(), name='upcoming'),
     path('<int:pk>/', include([
+        path('timetable/', login_required(patient_required(timetable.TimetableView.as_view())), name='timetable'),
         path('change/', patient.AppointmentUpdateView.as_view(), name='update'),
         path('cancel/', patient.AppointmentCancelView.as_view(), name='delete'),
         path('make/', patient.MakeAppointment.as_view(), name='make')
