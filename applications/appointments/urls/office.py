d8b924436bff034cf7fe7aa48111763cbc56f27d
szypkiwonsz <kacpersawicki321@gmail.com>
2020-12-17 19:05:19 +0100

    Add urls for services

--- applications/appointments/urls/office.py
+++ applications/appointments/urls/office.py
@@ -11,5 +11,13 @@ urlpatterns = [
         path('update/', office.AppointmentUpdateView.as_view(), name='update'),
         path('delete/', office.AppointmentDeleteView.as_view(), name='delete'),
         path('<str:date>/<str:service>/make/', office.MakeAppointment.as_view(), name='make')
+    ])),
+    path('services/', include([
+        path('', office.ServiceListView.as_view(), name='service_list'),
+        path('add', office.AddServiceView.as_view(), name='service_add'),
+        path('<int:pk>/', include([
+            path('edit', office.ServiceUpdateView.as_view(), name='service_edit'),
+            path('delete', office.ServiceDeleteView.as_view(), name='service_delete')
+        ])),
     ]))
 ]
