0c4afbdbab728e6247834f3197c19f8d9af420bd
szypkiwonsz <kacpersawicki321@gmail.com>
2021-01-06 14:35:16 +0100

    Adjust methods for appointment services

--- applications/office_panel/utils.py
+++ applications/office_panel/utils.py
@@ -1,7 +1,9 @@
 import calendar
 import datetime
 
-from applications.users.models import OfficeDay
+import pandas as pd
+
+from applications.users.models import OfficeDay, UserOffice
 from utils.add_zero import add_zero
 
 
@@ -14,22 +16,28 @@ def get_number_of_days_in_month(year, month):
     return days
 
 
-def get_hours_in_day(earliest_time, latest_time):
+def get_hours_in_day(earliest_time, latest_time, office_id):
     """A function that returns a list of hours between two given hours."""
-    hours = [f'{i}:00' for i in range(earliest_time, latest_time + 1)]
+    split_by = UserOffice.objects.values_list(
+        'appointment_time_interval', flat=True).filter(pk=office_id).first()
+    hours = []
+    for i in range(earliest_time, latest_time):
+        for j in range(0, 60, split_by):
+            hours.append(f'{i}:{add_zero(j)}')
     return hours
 
 
-def get_dates_in_month(request, days_in_month, month, year):
+def get_dates_in_month(office_id, days_in_month, month, year):
     """The function returns all possible dates of making an appointment to the office with the hours."""
     dates_in_month = []
     for day in days_in_month:
         dates = []
         date = datetime.datetime(int(year), int(month), int(day))
-        office_day = OfficeDay.objects.get(office=request.user.office, day=date.weekday())
+        office_day = OfficeDay.objects.get(office=office_id, day=date.weekday())
         hours_in_day = get_hours_in_day(
-            int(office_day.earliest_appointment_time.split(':')[0]),
-            int(office_day.latest_appointment_time.split(':')[0])
+            earliest_time=int(office_day.earliest_appointment_time.split(':')[0]),
+            latest_time=int(office_day.latest_appointment_time.split(':')[0]),
+            office_id=office_id
         )
         for hour in hours_in_day:
             date = f'{add_zero(day)}.{add_zero(month)}.{year} {hour}'
